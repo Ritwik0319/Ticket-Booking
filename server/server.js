6f287ae12ee3716ffcecb5fcd7833260e9fa6a5e
Ritwik0319 <ritwiksai1234@gmail.com>
2026-03-13 18:27:38 +0530

    added the verce url in cors

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://ticket-booking-88lf.onrender.com",
+      "https://ticket-booking-taupe-tau.vercel.app",
     ],
     credentials: true,
   }),
